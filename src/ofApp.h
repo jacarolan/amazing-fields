4e457918e67778b28e7224d1f21f960cb22663de
jacarolan <jacarolan22@gmail.com>
2018-11-08 11:59:22 -0600

    Began setting up open frameworks environment

---
+++ src/ofApp.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <ctime>
+#include <cstdlib>
+#include <utility>
+
+#include "ofMain.h"
+#include "snake.h"
+#include "SnakeFood.h"
+#include "ll.h"
+
+namespace af {
+
+class runner : public ofBaseApp {
+private:
+	ofImage background;
+
+	ofSoundPlayer change_dir_sound;
+	ofSoundPlayer music;
+
+	void drawBackground();
+
+	// Resets the game objects to their original state.
+	void reset();
+
+public:
+	// Function used for one time setup
+	void setup();
+
+	// Main event loop functions called on every frame
+	void update();
+	void draw();
+	
+	// Event driven functions, called on appropriate user action
+	void keyPressed(int key);
+	void windowResized(int w, int h);
+};
+} // namespace af
